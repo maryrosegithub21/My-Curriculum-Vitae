b33928dff9a32c111cb5bef99fc2973004886a4e
maryrosegithub21 <mary_rose_antonio@yahoo.com>
2024-06-04 19:58:34 +1200

    fix some virtual chat

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // ======= For Chat Form ====== //
+// ===== connection to html ===== //
     const chatList = document.getElementById("chatList");    
     const chatDiv = document.getElementById("chat-div");   
-    const chat = document.querySelector("input");
+    const chat = document.getElementById("input");
     const chatSpan = document.getElementById("inquiry");
     const contact = "My phone number is (027)210-8200";
     const email = "My email address is maryroseantoniowork21@gmail.com";
@@ -13,7 +14,7 @@
     chatList.style.justifyContent = "end";
     chatList.style.backgroundColor="white";
 
-    // ===== For Lets Chat Button
+    // ===== For Lets Chat Button ===== //
     function togglemenu(event) {
       event.preventDefault();
       if(chatList.style.maxHeight === "0px") 
@@ -49,77 +50,46 @@ function chatBook(event){
       calendar.style.height = "280px";
       calendar.style.width = "100%";
       chatList.style.maxHeight = "0px";
+      chatSpan.textContent = "";
     }
     else
     {
       calendar.style.height = "0px";
+      chatSpan.textContent = "";
     }
 }
 //  ======== Reply Condition to Virtual Chat ==== //
-
-function chatInquiry(event){
+function chatInquiry(event) {
   event.preventDefault();
-        if (chat.textContent = " "){
-          // chatList.style.maxHeight = "250px";
-          // chatList.style.maxWidth = "50%";
-          chatSpan.textContent = "Please select an option or typing your inquiry in english"; 
-          alert("Please select an option or typing your inquiry in english")
-      
-        } elseif (chat.textContent = "hi")
-        {
-          chatSpan.textContent = "Hello, How can I help you today or Please select option."
-          // chatSpan.textContent = "Please book appointment";
-        } elseif (chat.textContent = "how much")
-        chatSpan.textContent = "Its all Negotiable. Please book appointment for more information"
-        // chatSpan.textContent = "Please book appointment"; else {
-
-        
-      }
-    
-    
-  // // while (true){
-  
-  // if (chatSpan.textContent = "hi")
-  // {
-  //   chatSpan.textContent = "Hello, How can I help you today or Please select option."
-  // }
-  // if (chatSpan.textContent = "how much")
-  // {
-  //   chatSpan.textContent = "It's all Negotiable"
-  // }
-  // if (chatSpan.textContent = " " )
-  // {
-  //     chatSpan.textContent = "Please select an option or typing your inquiry in englsih";
-  //   // chatSpan.textContent = "Please book appointment";
-  // }
-  // // return
-// }
-// let x = 0;
-// while(true){
-//     if (x === 3){
-//         console.log("x is now 3")
-//     }
-//     if (x === 10){
-//         console.log("x is now 3")
-//     }
-//     if (x === 15){
-//         console.log("x is now 3")
-//     }
-// }
- 
-
-    // if (chat.textContent = " "){
-    //       // chatList.style.maxHeight = "250px";
-    //       // chatList.style.maxWidth = "50%";
-    //       chatSpan.textContent = "Please select an option or typing your inquiry in english";
-      
-    //     } else {
-    //       chatSpan.textContent = "Please book appointment";
-    //     }
-    //   }
-  
-// }
-//   
+  const ans = chat.value;
+  if (ans === "hi") {
+    chatSpan.textContent =
+      "Hello, How can I help you?";
+  } else if (ans === "hello") {
+    chatSpan.textContent =
+      "Hi, How can I help you?";
+  } else if (ans === "how much") {
+    chatSpan.textContent =
+      "Its all Negotiable. Please book appointment for more information";
+  } else if (ans === " ") {
+    chatSpan.textContent =
+      "Please select an option or typing your inquiry in english";
+  } else if (ans === "who are you?") {
+    chatSpan.textContent =
+      "I'm Mary bot.Please select an option or typing your inquiry in english";
+  } else if (ans === "how are you?") {
+    chatSpan.textContent =
+      "I'm Fine.Please select an option or typing your inquiry in english";
+  } else if (ans === "are you human") {
+    chatSpan.textContent =
+      "No, I'm Virtual Assisatant.Please select an option or typing your inquiry in english";
+  } else if (ans === "are you bot") {
+    chatSpan.textContent =
+      "Yes, I'm Virtual Assisatant.Please select an option or typing your inquiry in english";
+  } else {
+    chatSpan.textContent = "I don't get what you mean. Please select an option";
+  }
+}
 
  // ======== Portfolio slideshow ======== //
  let slideIndex = 1;
@@ -136,8 +106,12 @@ function chatInquiry(event){
    let i;
    let slides = document.getElementsByClassName("mySlides");
    let dots = document.getElementsByClassName("dot");
-   if (count > slides.length) {slideIndex = 1}    
-   if (count < 1) {slideIndex = slides.length}
+   if (count > slides.length) {
+    slideIndex = 1
+  }    
+   if (count < 1) {
+    slideIndex = slides.length
+  }
    for (i = 0; i < slides.length; i++) {
      slides[i].style.display = "none";  
    }
